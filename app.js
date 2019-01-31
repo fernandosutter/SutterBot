fb4c5d56e34393f6179e749c9162b706b15dec73
Fernando Sutter <fernando.sutter1@gmail.com>
2019-01-31 21:59:15 -0200

    Sends the message only for the other channels

--- app.js
+++ app.js
@@ -25,8 +25,11 @@ client.on('message', receivedMessage => {
 	// Check if the bot's user was tagged in the message
 	if (receivedMessage.content.includes('!all')) {
 		client.channels.forEach(channel => {
-			if (channel.type == 'text') {
-				receiverChannel = client.channels.get(channel.id);
+			if (
+				channel.type == 'text' &&
+				channel.id !== receivedMessage.channel.id
+			) {
+				let receiverChannel = client.channels.get(channel.id);
 				receiverChannel.send(
 					'Message sent from ' +
 						receivedMessage.author.toString() +
